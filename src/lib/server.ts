bf9a8dc1631330c1952d75ed45855c9b59889bf8
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-06-21 00:38:30 +0300

    url commit

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,3 +1,4 @@
+import { existsSync, readFileSync } from 'node:fs';
 import http, { IncomingMessage, ServerResponse } from 'node:http';
 
 type Server = {
@@ -12,31 +13,34 @@ server.httpServer = http.createServer((req: IncomingMessage, res: ServerResponse
     console.log(req.url);
 
     const isTextFile = req.url?.endsWith('.css') || req.url?.endsWith('.js') || req.url?.endsWith('.svg');
-    const isBinaryFile = req.url?.endsWith('.png') || req.url?.endsWith('.jpg') || req.url?.endsWith('.webp') || req.url?.endsWith('.eot') || req.url?.endsWith('.ttf');
+    const isBinaryFile = req.url?.endsWith('.ico') || req.url?.endsWith('.png') || req.url?.endsWith('.jpg') || req.url?.endsWith('.webp') || req.url?.endsWith('.eot') || req.url?.endsWith('.ttf');
     const isAPI = req.url?.startsWith('/api/');
     const isPage = !isTextFile && !isBinaryFile && !isAPI;
+    let content: any;
 
     if (isTextFile) {
         res.setHeader('Content-Type', 'text/plain');
-        res.end('Tekstinio failo turinys');
+        if (existsSync(process.env.PWD + '/public' + req.url)) {
+            content = readFileSync(process.env.PWD + '/public' + req.url);
+        }
     } else if (isBinaryFile) {
         res.setHeader('Content-Type', 'application/octet-stream');
-        res.end('Binarinio failo turinys');
+        content = 'Binarinio failo turinys';
     } else if (isAPI) {
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ message: 'API atsakymas' }));
+        content = JSON.stringify({ message: 'API atsakymas' });
     } else if (isPage) {
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<html><body><h1>Puslapio turinys</h1></body></html>');
-    } else if (req.url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-        const responseContent = `<!DOCTYPE html>
-        <!-- HTML content -->
-        `;
-        res.end(responseContent);
+        const pageFile = req.url === '/' ? 'index.html' : req.url?.substring(1).split('/').join('_') + '.html';
+        if (existsSync(process.env.PWD + '/public/pages/' + pageFile)) {
+            content = readFileSync(process.env.PWD  + '/public/pages/' + pageFile);
+        }
+    }
+
+    if (content) {
+        res.end(content);
     } else {
         res.statusCode = 404;
-        res.end('Klaida: Puslapis nerastas');
+        res.end(readFileSync(process.env.PWD + '/public/pages/not-found.html'));
     }
 
     // puslapio html
